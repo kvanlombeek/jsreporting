26a1223be7c99a73cdcc61cd04cce8469d59ea07
kvanlombeek <kasper.vanlombeek@gmail.com>
2025-08-08 16:43:28 +0200

    intial commit

---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    fontFamily: {
+      "pt-serif": ["PT Serif", "serif"],
+      montserrat: ["Montserrat", "sans-serif"],
+    },
+    backgroundSize: {
+      auto: "auto",
+      cover: "cover",
+      contain: "contain",
+      "100%": "100%",
+    },
+    extend: {
+      backgroundImage: {
+        underline1: "url('./assets/Underline1.svg')",
+        underline2: "url('./assets/Underline2.svg')",
+        underline3: "url('./assets/Underline2black.svg')",
+        underline4: "url('./assets/Underline4.svg')",
+        highlight3: "url('./assets/Highlight3.svg')",
+      },
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 0.5s ease-out",
+      },
+    },
+  },
+  variants: {
+    extend: {    
+      colors: {
+      secondary: "#F4F2ED",
+      black: "black",
+      white: "white",
+      },
+    },
+  },
+  plugins: [],
+};
